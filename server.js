46f10557b01e9b9027e3e0dafb84a3f7c5f8ff60
JBDooley <110794211+JBDooley@users.noreply.github.com>
2023-02-13 16:47:39 -0700

    Added server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+const notes = require('./db/db.json');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes.slice(1));
+});
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
